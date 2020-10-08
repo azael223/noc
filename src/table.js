ecfbbe5be89b139c5239cef8dcc6d599fcdf8add
Azael Rivera <azaelkiller@gmail.com>
2020-10-08 15:02:21 -0600

    Code optimization, table 2 added

--- src/table.js
+++ src/table.js
@@ -1,35 +1,75 @@
-/* Table manager */
-const addRow = (table) => {
-  table.addRow({ id: table["rowManager"]["rows"].length });
-};
-const addColumn = (table) => {
-  const id = "id_" + (table["columnManager"]["columns"].length + 1);
-  table.addColumn({ field: id });
-};
-const deleteRow = (table) => {
-  let rowsl = table["rowManager"]["rows"].length;
-  return (rowsl = rowsl > 3 ? table.deleteRow(rowsl - 1) : 0);
-};
-const deleteColumn = (table) => {
-  let columnsl = table["columnManager"]["columns"].length;
-  return columnsl > 3 ? table.deleteColumn("id_" + columnsl) : 0;
-};
+const TABLE1 = document.querySelector("#table1");
+const TABLE2 = document.querySelector("#table2");
 
-/* table init */
-var tablerows = [{}, {}, {}];
+/* Table Manager */
+const hideDiv = (divId) =>
+  document.querySelector(`#${divId}`).classList.add("hide");
+const showDiv = (divId) =>
+  document.querySelector(`#${divId}`).classList.remove("hide");
+const changeTable = () => {
+  if (TABLE2.classList.contains("hide")) {
+    hideDiv("table1");
+    showDiv("table2");
+  } else {
+    hideDiv("table2");
+    showDiv("table1");
+  }
+};
+/* *Row Manager*/
+const addRow = (table) => table.addRow({ id: getRowsSize(table) });
 
-var table1 = new Tabulator("#tabla1", {
+const deleteRow = (table) =>
+  getRowsSize(table) > 3 ? table.deleteRow(getRowsSize(table) - 1) : 0;
+
+const getRowsSize = (table) => table["rowManager"]["rows"].length;
+
+/* *Column Manager */
+const addColumn = (table) =>
+  table.addColumn({
+    field: `col${getColsSize(table)}`,
+    title: `Value ${getColsSize(table)}`,
+    editableTitle: true,
+  });
+
+const deleteColumn = (table) =>
+  getColsSize(table) > 3
+    ? table.deleteColumn(`col${getColsSize(table) - 1}`)
+    : 0;
+
+const getColsSize = (table) => table["columnManager"]["columns"].length;
+
+/* table1 init */
+var table1rows = [{ id: 1 }, { id: 2 }];
+var table1cols = [
+  { title: "id" },
+  { editor: "number", editableTitle: true, title: "Value 1" },
+  { editor: "number", editableTitle: true, title: "Value 2" },
+];
+var table1 = new Tabulator("#table1", {
   headerVisible: true,
-  headerSort:false,
+  headerSort: false,
   height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-  data: tablerows, //assign data to table
+  data: table1rows, //assign data to table
   layout: "fitColumns", //fit columns to width of table (optional)
-
-  columns: [
-    //Define Table Columns
-    {editor:"input",editableTitle:false,title:"id"},
-
-    {editor:"number",editableTitle:true,title:"Value 1"},
-    {editor:"number",editableTitle:true,title:"Value 2"},
-  ],
+  columns: table1cols,
 });
+
+/* table2 init */
+var table2rows = [{ id: 1 }, { id: 2 }];
+var table2cols = [
+  { title: "values" },
+  { title: "Maximin" },
+  { title: "Laplace" },
+  { title: "Optim-pesim" },
+  { title: "Minimax" },
+];
+var table2 = new Tabulator("#table2", {
+  headerVisible: true,
+  headerSort: false,
+  height: 205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+  data: table2rows, //assign data to table
+  layout: "fitColumns", //fit columns to width of table (optional)
+  columns: table2cols,
+});
+
+hideDiv("table2");
